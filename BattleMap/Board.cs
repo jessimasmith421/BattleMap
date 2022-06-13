02154bf68fc70edb38970c23e6edf8be809163db
Jessi Austin <jessimasmith.421@gmail.com>
2022-06-13 15:04:25 -0400

    beginning method for battlemap generation

---
+++ BattleMap/Board.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleMap
+{
+    public class Board
+    {
+        //method
+
+        public void MakeBoardTop(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("__");
+
+            }
+            Console.WriteLine("_");
+        }
+        public void MakeBoard(int height, int length)
+        {
+            MakeBoardTop(length);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+
+                    //Console.WriteLine("-");
+                    //Console.WriteLine("");
+
+                    Console.Write("|_");
+                }
+                Console.WriteLine("|");
+            }
+        }
+
+        public void MakeBoardTakeTwo(int height, int length)
+        {
+            MakeBoardTop(length);
+            for (int i = 0; i < height; i++)
+            {
+                for (int k = 0; k < length; k++)
+                {
+
+                    Console.Write("| \t");
+
+                }
+                Console.WriteLine();
+                for (int k = 0; k < length; k++)
+                {
+
+                    Console.Write("| \t");
+
+                }
+                Console.WriteLine();
+                for (int j = 0; j < length; j++)
+                {
+
+                    Console.Write("|_______");
+                }
+                Console.WriteLine("|");
+                Console.WriteLine("|");
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- BattleMap/Program.cs
+++ BattleMap/Program.cs
@@ -7,6 +7,16 @@ namespace BattleMap
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO THE THUNDERDOME");
+            Console.Write("How long do you want your map to be? ");
+            string length = Console.ReadLine();
+            int lengthInt = int.Parse(length);
+            Console.Write("How tall homie? ");
+            string height = Console.ReadLine();
+            int heightInt = int.Parse(height);
+
+            Board board = new Board();
+            board.MakeBoard(heightInt, lengthInt);
+
         }
     }
 }
